344222fc1b27f09fd6aa155ceab45f325a6a023e
parkminwoo <parkminwoo1991@gmail.com>
2023-02-27 17:54:44 +0900

    feat: update quickshow packages

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# quickshow
-Quick-Show provides simple but powerful insight plots.
+# Quick-Show
+Quick-Show is a package that allows you to easily and quickly draw 2D or 3D t-SNE and pca plots using specific columns of a refined dataframe.
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quickshow",
+    version="0.1.0",
+    author="parkminwoo",
+    author_email="parkminwoo1991@gmail.com",
+    description="Quick-Show provides simply but powerful insight plots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DSDanielPark/quickshow",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6')
