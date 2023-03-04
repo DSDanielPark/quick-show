7804fd2ebf79287d499fe8af41d658b8726f0a90
parkminwoo <parkminwoo1991@gmail.com>
2023-03-04 20:01:01 +0900

    feat: update utils rcparam

--- quickshow/utils.py
+++ quickshow/utils.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import os
 
 
@@ -10,3 +11,13 @@ def find_all_files(folder_path:str, include_word:str) -> list:
                 all_file_list.append(file_path)
 
     return all_file_list
+
+
+def rcparam(**args):
+    plt.style.use('tableau-colorblind10')
+    plt.rcParams["figure.figsize"] = (20,10)
+    plt.rcParams['lines.linewidth'] = 4
+    plt.rcParams['axes.grid'] = False 
+    plt.rcParams['axes.facecolor'] = 'white'
+
+        
