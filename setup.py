c6b8b0ba98401b16e83ad17853708c5c21870c14
parkminwoo <parkminwoo1991@gmail.com>
2023-03-05 18:37:19 +0900

    build: 0.1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quickshow",
-    version="0.1.9",
+    version="0.1.10",
     author="parkminwoo",
     author_email="parkminwoo1991@gmail.com",
     description="Quick-Show provides simply but powerful insight plots",
