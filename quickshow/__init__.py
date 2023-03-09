3cce8441ede32a7491e5c6dca08de13ae3802f53
parkminwoo <parkminwoo1991@gmail.com>
2023-03-09 23:24:53 +0900

    build: 0.1.11

--- quickshow/__init__.py
+++ quickshow/__init__.py
@@ -22,4 +22,4 @@ from quickshow.utils import find_all_files
 __all__ = ["vis_tsne2d", "vis_tsne3d", "vis_pca", "vis_cluster_plot", "vis_cm"
            ,"get_total_cr_df", "vis_multi_plot", "find_all_files", "rcparams"]
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
